6099e20b5c72c7c8996134455b834f0e12ffc166
neroHua <2676337735@qq.com>
2022-02-04 19:07:32 +0800

    add OrganizingData

---
+++ src/chapter08/OrganizingData.java
@@ -0,0 +1,40 @@
+package chapter08;
+
+/**
+ * 1. Self Encapsulate Field
+ *
+ * 2. Replace Data Value with Object
+ *
+ * 3. Change Value to Reference
+ *
+ * 4. Change Reference to Value
+ *
+ * 5. Replace Array with Object
+ *
+ * 6. Duplicate Observed Data
+ *
+ * 7. Change Unidirectional Association to Bidirectional
+ *
+ * 8. Change Bidirectional Association to Unidirectional
+ *
+ * 9. Replace Magic Number with Symbolic Constant
+ *    这个简直是最毒的规则了吧
+ *
+ * 10. Encapsulate Field
+ *
+ * 11. Encapsulate Collection
+ *
+ * 12. Replace Record with Data Class
+ *
+ * 13. Replace Type Code with Class
+ *
+ * 14. Replace Type Code with Subclasses
+ *
+ * 15. Replace Type Code with State/Strategy
+ *     13,14,15很相似根据情况和喜好进行选取吧
+ *
+ * 16. Replace Subclass with Fields
+ *
+ */
+public class OrganizingData {
+}

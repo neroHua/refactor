0685903eb0fe6716c24fb786736f1c641ab66818
neroHua <2676337735@qq.com>
2022-02-04 19:33:33 +0800

    add DealingWithGeneralization

---
+++ src/chapter11/DealingWithGeneralization.java
@@ -0,0 +1,30 @@
+package chapter11;
+
+/**
+ * 1. Pull Up Field
+ *
+ * 2. Pull Up Method
+ *
+ * 3. Pull Up Constructor Body
+ *
+ * 4. Push Down Method
+ *
+ * 5. Push Down Field
+ *
+ * 6. Extract Subclass
+ *
+ * 7. Extract Superclass
+ *
+ * 8. Extract Interface
+ *
+ * 9. Collapse Hierarchy
+ *
+ * 10. Form Template Method
+ *
+ * 11. Replace Inheritance with Delegation
+ *
+ * 12. Replace Delegation with Inheritance
+ *
+ */
+public class DealingWithGeneralization {
+}

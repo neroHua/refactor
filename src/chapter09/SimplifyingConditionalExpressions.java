980b09a26c3b10a1b4e5b03f974b76fef480ea43
neroHua <2676337735@qq.com>
2022-02-04 19:19:51 +0800

    add SimplifyingConditionalExpressions

---
+++ src/chapter09/SimplifyingConditionalExpressions.java
@@ -0,0 +1,27 @@
+package chapter09;
+
+/**
+ * 1. Decompose Conditional
+ *      条件越是复杂，越是需要一个有意义的名字。
+ *      不过，条件越是复杂，系统也就越垃圾，为什么会出现这种情况？
+ *
+ * 2. Consolidate Conditional Expression
+ *
+ * 3. Consolidate Duplicate Conditional Fragments
+ *
+ * 4. Remove Control Flag
+ *
+ * 5. Replace Nested Conditional with Guard Clauses
+ *    这是一个很好的减少条件嵌套层次的好办法
+ *
+ * 6. Replace Conditional with Polymorphism
+ *
+ * 7. Introduce Null Object
+ *    这也可以称之为一个设计模式吧
+ *
+ * 8. Introduce Null Object
+ *
+ * 9. Introduce Assertion
+ */
+public class SimplifyingConditionalExpressions {
+}

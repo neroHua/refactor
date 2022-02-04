f0e40c19ff4c1447c933927fbe78a5538efa1e43
neroHua <2676337735@qq.com>
2022-02-04 18:58:51 +0800

    add MovingFeatureBetweenObject

---
+++ src/chapter07/MovingFeatureBetweenObject.java
@@ -0,0 +1,22 @@
+package chapter07;
+
+/**
+ * 1. Move Method
+ *
+ * 2. Move Field
+ *
+ * 3. Extract Class
+ *
+ * 4. Inline Class
+ *
+ * 5. Hide Delegate
+ *
+ * 6. Remove Middle Man
+ *
+ * 7. Introduce Foreign Method
+ *
+ * 8. Introduce Local Extension
+ *
+ */
+public class MovingFeatureBetweenObject {
+}

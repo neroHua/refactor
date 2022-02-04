287f9200ef39f2cf641be567c6efb6ea7d87287f
neroHua <2676337735@qq.com>
2022-02-04 19:28:00 +0800

    add MakingMethodCallSimpler

---
+++ src/chapter10/MakingMethodCallSimpler.java
@@ -0,0 +1,40 @@
+package chapter10;
+
+/**
+ * 下面的这些方法，应该会是经常使用的功能了吧
+ *
+ * 1. Rename Method
+ *
+ * 2. Add Parameter
+ *
+ * 3. Remove Parameter
+ *
+ * 4. Separate Query from Modifier
+ *
+ * 5. Parameterize Method
+ *
+ * 6. Replace Parameter with Explicit Methods
+ *
+ * 7. Preserve Whole Object
+ *
+ * 8. Replace Parameter with Method
+ *
+ * 9. Introduce Parameter Object
+ *
+ * 10. Remove Setting Method
+ *
+ * 11. Hide Method
+ *
+ * 12. Replace Constructor with Factory Method
+ *
+ * 13. Encapsulate Downcast
+ *     也可以通过泛型进行参数化
+ *
+ * 14. Replace Error Code with Exception
+ *
+ * 15. Replace Exception with Test
+ *      这个我这里依然持保留意见
+ *
+ */
+public class MakingMethodCallSimpler {
+}

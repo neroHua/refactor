960e8d34b66beee07a9dddbfd0166cead36d2419
neroHua <2676337735@qq.com>
2022-02-04 18:54:03 +0800

    add composing method

---
+++ src/chapter06/ComposingMethod.java
@@ -0,0 +1,25 @@
+package chapter06;
+
+/**
+ * 1. Extract Method
+ *
+ * 2. Inline Method
+ *
+ * 3. Inline Temp
+ *
+ * 4. Replace Temp with Query
+ *
+ * 5. Introduce Explaining Variable
+ *
+ * 6. Split Temporary Variable
+ *
+ * 7. Remove Assignments to Parameters
+ *    这个应该还是很重要的，尤其是入参不能改变的情况下
+ *
+ * 8. Replace Method with Method Object
+ *
+ * 9. Substitute Algorithm
+ *
+ */
+public class ComposingMethod {
+}
